a8beab7e72e488e038c69c094e1929bf042c03f5
Jean-Rubin <rubin.jean98@gmail.com>
2022-04-10 20:29:48 +0200

    Changed test names

--- test/test_sudoku.ml
+++ test/test_sudoku.ml
@@ -5,21 +5,21 @@ let test_correct1 () =
   Alcotest.(check bool) "Trivial solution is correct"
     true (Sudoku.correct solution1)
 
-let test_correct2 () =
-  let solution2 = Sudoku.read "data/test2_sol.txt" in
-  Alcotest.(check bool) "9x9 Solution is correct"
+let test_correct3 () =
+  let solution2 = Sudoku.read "data/test3_sol.txt" in
+  Alcotest.(check bool) "Simple solution is correct"
     (Sudoku.correct solution2) (true)
 
 let test_board1 () =
   let test1 = Sudoku.read "data/test1.txt" in
   let solution1 = Sudoku.read "data/test1_sol.txt" in
-  Alcotest.(check sudoku_testable) "Trivial Sudoku is solved"
+  Alcotest.(check sudoku_testable) "Trivial sudoku is solved"
     solution1 (List.hd (Sudoku.sudoku test1)) 
 
 (* let test_board2 () = *)
 (*   let test2 = Sudoku.read "data/test2.txt" in *)
 (*   let solution2 = Sudoku.read "data/test2_sol.txt" in *)
-(*   Alcotest.(check sudoku_testable) "Simple 9x9 Sudoku is solved" *)
+(*   Alcotest.(check sudoku_testable) "Simple sudoku is solved" *)
 (*     (List.hd (Sudoku.sudoku test2)) (solution2) *)
 
 let () =
@@ -29,12 +29,12 @@ let () =
       [
         Alcotest.test_case "Correct Trivial" `Quick
           test_correct1;
-        Alcotest.test_case "Correct 9x9" `Quick
-          test_correct2;
+        Alcotest.test_case "Correct Simple" `Quick
+          test_correct3;
       ]);
       ("Solving small sudoku",
       [
-        Alcotest.test_case "Simple Trivial" `Quick
+        Alcotest.test_case "Solve Trivial" `Quick
           test_board1;
       ])
     ]
