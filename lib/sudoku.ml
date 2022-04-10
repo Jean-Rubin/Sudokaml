9154bf56e41f6a756b8eca79f44e9654edea107d
Jean-Rubin <rubin.jean98@gmail.com>
2022-04-10 03:09:57 +0200

    Now boards can be n^2 x n^2

--- lib/sudoku.ml
+++ lib/sudoku.ml
@@ -1,21 +1,22 @@
 type +'a matrix = 'a list list
 (* The matrix is given by a list of its rows *)
-(* type choices = char list *)
+type choices = char list
 
-type board = char matrix
-
-let box_size = 2
-let cell_vals = ['1'; '2'; '3'; '4']
+type board = {
+  grid : char matrix;
+  box_size : int;
+  cell_vals : choices
+}
 
 let blank chr = (chr = '.')
 
 let equal board1 board2 =
-  List.equal (List.equal ( = )) board1 board2
+  List.equal (List.equal ( = )) board1.grid board2.grid
 
 let print board =
   let print_line line =
     List.iter (fun e -> print_char e; print_char ' ') line in
-  List.iter (fun line -> print_line line; print_newline () ) board;;
+  List.iter (fun line -> print_line line; print_newline () ) board.grid;;
 
 let pp_print ppf board =
   let open Format in
@@ -24,30 +25,48 @@ let pp_print ppf board =
     fprintf ppf "@[<h>%a@]"
       (pp_print_list ~pp_sep:space pp_print_char) line
   in
-  pp_print_list ~pp_sep:pp_force_newline pp_print_line ppf board;;
+  pp_print_list ~pp_sep:pp_force_newline pp_print_line ppf board.grid;;
 
 let read filepath =
   let to_char = List.map (fun s -> String.get s 0) in
   let in_channel = open_in filepath in
-  let rec reader board_acc in_channel =
+  let read_cell in_channel =
+    try
+      let line = input_line in_channel in
+      let cell_vals = List.init (String.length line) (String.get line) in
+      let _ = input_line in_channel in
+      cell_vals
+    with
+      | e ->
+      close_in in_channel;
+      raise e;
+  in
+  let rec read_grid grid_acc in_channel =
   try 
     let line = input_line in_channel in
-    let board_line = to_char (String.split_on_char ' ' line) in
-    reader (board_line :: board_acc) in_channel
+    let grid_line = to_char (String.split_on_char ' ' line) in
+    read_grid (grid_line :: grid_acc) in_channel
   with
   | End_of_file ->
     close_in in_channel;
-    List.rev board_acc
+    List.rev grid_acc
   | e ->
     close_in in_channel;
     raise e;
-  in reader [] in_channel
+  in 
+  let cell_vals = read_cell in_channel in
+  let grid = read_grid [] in_channel in
+  {
+    grid = grid;
+    box_size = List.length cell_vals |> float_of_int |> sqrt |> int_of_float;
+    cell_vals = cell_vals;
+  }
 
 let rec nodups = function
   | [] -> true
   | x::xs -> not (List.mem x xs) && nodups xs
 
-let rows board = board
+let rows grid = grid
 
 let rec cols = function
   | [] -> []
@@ -61,23 +80,23 @@ let group_by n lst =
   | x::xs -> aux (k + 1) (x :: acc) acc_total xs
   in aux 0 [] [] lst
 
-let group lst = group_by box_size lst
-let ungroup board = List.concat board
+let group board lst = group_by board.box_size lst
+let ungroup grid = List.concat grid
 
 let boxs board =
-  List.map group board |>
-  group |>
+  List.map (group board) board.grid |>
+  group board |>
   List.map cols |>
   ungroup |>
   List.map ungroup
 
 let correct board = 
-  List.for_all nodups (rows board) &&
-  List.for_all nodups (cols board) &&
+  List.for_all nodups (rows board.grid) &&
+  List.for_all nodups (cols board.grid) &&
   List.for_all nodups (boxs board)
 
-let choose e = if blank e then cell_vals else [e]
-let choices board = List.(map (map choose)) board
+let choose board e = if blank e then board.cell_vals else [e]
+let choices board = List.(map (map (choose board))) board.grid
 
 let rec cp = 
   let outer_concat lst1 lst2 =
@@ -93,4 +112,9 @@ let matrix_cartesian_product choices = cp (List.map cp choices)
 let sudoku board =
   choices board |>
   matrix_cartesian_product |>
+  List.map (fun grid -> {
+    grid = grid;
+    box_size = board.box_size;
+    cell_vals = board.cell_vals;
+  }) |>
   List.filter correct
--- test/test_sudoku.ml
+++ test/test_sudoku.ml
@@ -1,15 +1,26 @@
 let sudoku_testable = Alcotest.testable Sudoku.pp_print Sudoku.equal
 
 let test_correct1 () =
-  let solution1 = Sudoku.read "data/test1sol.txt" in
-  Alcotest.(check bool) "Solution is correct"
+  let solution1 = Sudoku.read "data/test1_sol.txt" in
+  Alcotest.(check bool) "4x4 Solution is correct"
     (Sudoku.correct solution1) (true)
+
+let test_correct2 () =
+  let solution2 = Sudoku.read "data/test2_sol.txt" in
+  Alcotest.(check bool) "9x9 Solution is correct"
+    (Sudoku.correct solution2) (true)
 
 let test_board1 () =
   let test1 = Sudoku.read "data/test1.txt" in
-  let solution1 = Sudoku.read "data/test1sol.txt" in
-  Alcotest.(check sudoku_testable) "Simple Sudoku is solved"
+  let solution1 = Sudoku.read "data/test1_sol.txt" in
+  Alcotest.(check sudoku_testable) "Simple 4x4 Sudoku is solved"
     (List.hd (Sudoku.sudoku test1)) (solution1)
+
+let test_board2 () =
+  let test2 = Sudoku.read "data/test2.txt" in
+  let solution2 = Sudoku.read "data/test2_sol.txt" in
+  Alcotest.(check sudoku_testable) "Simple 9x9 Sudoku is solved"
+    (List.hd (Sudoku.sudoku test2)) (solution2)
 
 let () =
   Alcotest.run "Sudoku test functions"
@@ -18,6 +29,8 @@ let () =
       [
         Alcotest.test_case "Correct 4x4" `Quick
           test_correct1;
+        Alcotest.test_case "Correct 9x9" `Quick
+          test_correct2;
       ]);
       ("Solving small sudoku",
       [
